acd51333d2cd5da8187ed8347858cb6a8c5dfee2
Martyn Janes <martyn@obany.com>
2018-06-29 15:56:56 +0300

    Added factory

---
+++ src/factories/qrRendererFactory.ts
@@ -0,0 +1,34 @@
+import { FactoryBase } from "@tangle-frost/iota-core/dist/factories/factoryBase";
+import { IQRRenderer } from "../models/IQRRenderer";
+
+/**
+ * Factory to generate QR renderers.
+ */
+export class QRRendererFactory extends FactoryBase<IQRRenderer> {
+    /* @internal */
+    private static _instance: QRRendererFactory;
+
+    /**
+     * Don't allow manual construction of the factory.
+     * @internal
+     */
+    private constructor() {
+        super();
+    }
+
+    /**
+     * Get the instance of the factory.
+     * @returns The factory instance.
+     */
+    public static instance(): FactoryBase<IQRRenderer> {
+        if (!QRRendererFactory._instance) {
+            QRRendererFactory._instance = new QRRendererFactory();
+        }
+        return QRRendererFactory._instance;
+    }
+
+    /* @internal */
+    protected getInstance(): FactoryBase<IQRRenderer> {
+        return QRRendererFactory.instance();
+    }
+}
